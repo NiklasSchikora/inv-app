e46a1edc51cd67bbac0686d3c22e6ef8dbb320fe
Niklas Schikora <schikora@dasachtegebot.de>
2022-02-08 20:58:34 +0100

    Added Dexie.js and implemented update caching

--- js/addItem.js
+++ js/addItem.js
@@ -9,6 +9,49 @@ document.getElementById("addButton").addEventListener("click", async () => {
   let type = document.getElementById("db-type").value;
   let branch = document.getElementById("db-branch").value;
   console.log(name + " " + pnum + " " + snum + " " + type + " " + branch);
-  let code = await addItemToDb(name, pnum, snum, type, branch);
-  generateBarcode(code);
+  if (navigator.onLine) {
+    // Falls eine Verbindung besteht, wird das Item sofort hinzugefügt
+    let code = await addItemToDb(name, pnum, snum, type, branch);
+    generateBarcode(code);
+  } else {
+    // Falls der Status offline ist wird das neue Item Gecached und später synchronisiert
+    cacheAddItem(name, pnum, snum, type, branch);
+  }
+});
+
+var db = new Dexie("ItemCache");
+db.version(1).stores({
+  updates: `
+    ++id,
+    name,
+    pnum,
+    snum,
+    type,
+    branch
+  `,
+});
+
+const cacheAddItem = (name, pnum, snum, type, branch) => {
+  db.updates.put({
+    name: name,
+    pnum: pnum,
+    snum: snum,
+    type: type,
+    branch: branch,
+  });
+};
+
+document.getElementById("syncButton").addEventListener("click", async () => {
+  let updates = await db.updates.toArray();
+  updates.forEach((update) => {
+    console.log(update);
+    db.updates.where("id").equals(update.id).delete();
+    addItemToDb(
+      update.name,
+      update.pnum,
+      update.snum,
+      update.type,
+      update.branch
+    );
+  });
 });
