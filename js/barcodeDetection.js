cc29259ba3d2d059aaca8a54529e327868e6f64e
Niklas Schikora <schikora@dasachtegebot.de>
2022-02-15 17:22:15 +0100

    Added facing-mode

--- js/barcodeDetection.js
+++ js/barcodeDetection.js
@@ -18,7 +18,12 @@ import { getItemFromDb } from "./database.js";
     startbutton = document.getElementById("startbutton");
 
     navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
+      .getUserMedia({
+        video: {
+          facingMode: "environment",
+        },
+        audio: false,
+      })
       .then(function (stream) {
         video.srcObject = stream;
         video.play();
